4e7dd986d0b548cff92551e097a8411216da40fc
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-05-12 11:56:11 +0200

    Added user register functionality

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), {'template_name': 'users/login.html'}, name='login'),
     # Logout page.
     path('logout/', views.logout_view, name='logout'),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
