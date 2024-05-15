1653aba72137abff043628a3f6af46747f07b42e
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-05-15 15:57:27 +0200

    Added restriction to add or edit topic with implementation of '@login_required' in 'models.py', 'settings.py' and 'views.py'

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def index(request):
     """The home page for learning log."""
     return render(request, 'learning_logs/index.html')
@@ -14,6 +16,7 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required()
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -21,6 +24,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required()
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -36,6 +40,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required()
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -55,6 +60,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required()
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
